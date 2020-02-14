0ac4311eee8f13b61b706a44815d883ce378b871
Németh Nimród <nemethnimrod1997@gmail.com>
2020-02-14 12:07:14 +0100

    v.0.2.1 alpha
    getter/setter product.java

---
+++ Felulet/src/Tube/Product.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tube;
+
+/**
+ *
+ * @author nimrod
+ */
+public class Product {
+    private Object b;
+    private Object [][] r;
+    private Object [] p;
+    public Product() {
+        
+    }
+    
+    public Object getB() {
+        return b;
+    }
+
+    public void setB(Object b) {
+        this.b = b;
+    }
+
+    public Object[][] getR() {
+        return r;
+    }
+
+    public void setR(Object[][] r) {
+        this.r = r;
+    }
+
+    public Object[]getP() {
+        return p;
+    }
+
+    public void setP(Object[] r) {
+        this.p = p;
+    }
+    
+    
+}
